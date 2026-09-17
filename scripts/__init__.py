b19e30d915ed3869f647be71c5f7bb889b3b4a8a
Nat Osit <nat.a.osit@gmail.com>
2026-09-17 15:12:18 -0400

    Make scripts importable as a package

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+"""PIQITT script modules.
+
+This file makes the scripts directory an explicit Python package so shared
+implementation modules can be imported consistently by tests and other tools.
+"""
